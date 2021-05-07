e4d80b5c779148c1681a98b38a6d6093857e279c
Dara Yve <darayve@gmail.com>
2021-05-06 22:50:37 -0300

    Primeiras configurações

---
+++ app/src/main/java/com/example/lottoapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.lottoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView appTitle;
+    private TextView instructions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewsById();
+    }
+
+    private void findViewsById() {
+        appTitle = findViewById(R.id.main_title);
+        instructions = findViewById(R.id.instructions);
+    }
+}
